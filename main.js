24d4784b536fcfed5e0549340575aafe7b208ab5
Lexthus <Lexthus@gmail.com>
2023-05-23 20:39:05 -0300

    implementacion de scroll reveal

---
+++ main.js
@@ -0,0 +1,9 @@
+ScrollReveal().reveal('.fade-in-scroll', {
+    beforeReveal: function (el) {
+      el.style.opacity = 1;
+    },
+    distance: '50px',
+    duration: 800,
+    easing: 'ease-in'
+  });
+  
