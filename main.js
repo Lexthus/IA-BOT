65a343a96e9bab86f11bad2e85739f742ba39896
Lexthus <Lexthus@gmail.com>
2023-06-04 04:31:15 -0300

    nav-bar terminado

--- main.js
+++ main.js
@@ -1,20 +1,24 @@
-ScrollReveal().reveal('.fade-in-scroll', {
-  beforeReveal: function (el) {
-    el.style.opacity = 1;
-  },
-  distance: '50px',
-  duration: 800,
-  easing: 'ease-in'
+document.addEventListener("DOMContentLoaded", function() {
+  // Configuración de ScrollReveal
+  ScrollReveal().reveal('.fade-in-scroll', {
+    beforeReveal: function (el) {
+      el.style.opacity = 1;
+    },
+    distance: '50px',
+    duration: 800,
+    easing: 'ease-in'
+  });
+
+  const nav = document.querySelector("#navbar-menu");
+  const abrir = document.querySelector("#abrir-menu");
+  const cerrar = document.querySelector("#cerrar-menu");
+  
+  abrir.addEventListener ("click", () => {
+    nav.classList.add("visible")
+  })
+
+  cerrar.addEventListener ("click", () => {
+    nav.classList.remove("visible")
+  })
+  
 });
-
-document.querySelector(".burger").addEventListener("click", animateBars);
-
-var line1__bars = document.querySelector(".line1__bars-menu");
-var line2__bars = document.querySelector(".line2__bars-menu");
-var line3__bars = document.querySelector(".line3__bars-menu");
-
-function animateBars(){
-  line1__bars.classList.toggle("activeline1__bars-menu");
-  line2__bars.classList.toggle("activeline2__bars-menu");
-  line3__bars.classList.toggle("activeline3__bars-menu");
-}
